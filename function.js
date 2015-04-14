33c953158222227fa144f869ad3efecec7c3a58e
michael-thorpe <michael.thorpe@shopify.com>
2015-04-14 13:46:00 -0400

    update file

--- function.js
+++ function.js
@@ -1,8 +1,9 @@
 $(function(){
         $(".element").typed({
-            strings: ["shoes^1000", "clothes^1000", "hats^1000", "jewelry^1000", "tickets^1000", "handcrafts^1000", "photography^1000", "anything"],
+            strings: ["shoes^1000", "clothes^1000", "hats^1000", "jewelry^1000", "tickets^1000", "handcrafts^1000", "photography^1000", "handmade cards"],
             typeSpeed: 100,
             backSpeed: 100,
             backDelay: 800,
+            loop: true,
         });
     });
