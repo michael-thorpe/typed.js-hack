4f774ce99e9346c161cb5ecba8458bf6974af733
michael-thorpe <michael.thorpe@shopify.com>
2015-04-14 14:21:07 -0400

    uodate function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 $(function(){
         $(".element").typed({
-            strings: ["shoes^1000", "clothes^1000", "hats^1000", "jewelry^1000", "tickets^1000", "handcrafts^1000", "photography^1000", "handmade cards"],
+            strings: ["shoes^1000", "clothes^1000", "hats^1000", "jewelry^1000", "tickets^1000", "handcrafts^1000", "photography^1000", "handmade cards^1000"],
             typeSpeed: 100,
             backSpeed: 100,
             backDelay: 800,
