519a04464c8a8e8395907588abd05b96c7594887
michael-thorpe <michael.thorpe@shopify.com>
2016-05-18 10:23:22 -0400

    Update flipboard.js

--- flipboard.js
+++ flipboard.js
@@ -1,6 +1,5 @@
 
 var ref = document.referrer;
 if (ref.match(/^https?:\/\/([^\/]+\.)?flipboard\.com(\/|$)/i)) {
-  var div = document.getElementById('flipboard');
-  div.style.visibility = 'visible';
+  document.getElementById("flipboard").className = "";
 }
