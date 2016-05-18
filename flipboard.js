c4930b5b6ba4f4cf004c843c4e59bc243305237e
michael-thorpe <michael.thorpe@shopify.com>
2016-05-18 09:56:05 -0400

    updated to flipboard

---
+++ flipboard.js
@@ -0,0 +1,10 @@
+
+var ref = document.referrer;
+if (ref.match(/^https?:\/\/([^\/]+\.)?flipboard\.com(\/|$)/i)) {
+  var div = document.getElementById('flipboard');
+  div.style.visibility = 'visible';
+}
+else {
+	
+	div.style.visibility = 'hidden';
+}
